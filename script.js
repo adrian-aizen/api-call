20295f9299787acfd846c0ab79a6c92acfb3953b
Adrian Adora <aidss24adora@gmail.com>
2024-11-10 16:00:01 +0800

    Redefined JS codes

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-fetch('https://animechan.io/api/v1/quotes/random')
-    .then(response => response.json())
-    .then(quote => console.log(quote))
+function getRandomAnimeQuote() {
+    fetch('https://animechan.io/api/v1/quotes/random')
+      .then(response => response.json())
+      .then(quote => {
+        console.log(quote); // Log the entire quote object to the console
+        
+        // Populate the quote container with the fetched data
+        document.getElementById('quote-text').innerText = `"${quote.quote}"`;
+        document.getElementById('quote-character').innerText = `- ${quote.character}`;
+        document.getElementById('quote-anime').innerText = `from ${quote.anime}`;
+        
+        // Show the quotes container
+        const quotesContainer = document.getElementById('quotes-container');
+        quotesContainer.classList.remove('hidden');
+      })
+      .catch(error => {
+        console.error('Error fetching quote:', error);
+      });
+}
